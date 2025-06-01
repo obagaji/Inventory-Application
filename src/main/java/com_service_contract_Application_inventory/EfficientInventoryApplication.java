b898ca69478ba05c52b944ae4486cb968b1431a0
Musa Daniel <jtccontractsservicesng@yahoo.com>
2025-06-01 21:50:28 +0100

    The first commit for this project. Database, JPA are added

---
+++ src/main/java/com_service_contract_Application_inventory/EfficientInventoryApplication.java
@@ -0,0 +1,13 @@
+package com_service_contract_Application_inventory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfficientInventoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfficientInventoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com_service_contract_Application_inventory/EfficientInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com_service_contract_Application_inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfficientInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
